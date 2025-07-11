1b33c884175a188f3fd2341d4cf65d1189db486f
joaosantoss12 <souoduda2002@gmail.com>
2025-07-11 20:18:42 +0100

    initial commit - frontend

--- src/components/LeftSide.tsx
+++ src/components/LeftSide.tsx
@@ -40,7 +40,7 @@ const Icons = {
   messages: (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
       <rect x="3" y="5" width="18" height="14" rx="2"></rect>
-      <polyline points="3 7 12 13 21 7"></polyline>
+  
     </svg>
   ),
   live: (
